46651dae56fc8277cb53a915a685725f57858ae3
Alistair Blake <consultations@me.com>
2020-07-10 17:54:02 -0700

    prob

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 'use strict';
-let i= 0;
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let expectedSum = sum(sum(a, b)[0], c)[0];
+  let sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + expectedSum + '.';
+  let expectedProduct = multiply(multiply(a, b)[0], c)[0];
+  let productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + expectedProduct + '.';
+  return [expectedSum, expectedProduct, sumMessage, productMessage];
+};
+
+
+
+
 
